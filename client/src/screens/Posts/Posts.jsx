2d824bfe82ce4daf2cdb9d46945faf07692887bf
srhodges <89659625+srhodges@users.noreply.github.com>
2021-10-12 16:33:47 -0700

    save

--- client/src/screens/Posts/Posts.jsx
+++ client/src/screens/Posts/Posts.jsx
@@ -1,40 +1,16 @@
 // call search in this component. Nav search post mvpimport { useState, useEffect } from 'react'
 import './Posts.css'
-import Search from '../../components/Search/Search'
 import PostCard from '../../components/PostCard/PostCard'
 import Layout from '../../components/Layout/Layout'
-import { getPosts } from '../../services/posts'
+
 
 const Posts = (props) => {
 
   const { searchResult, posts } = props
-  
-  // const [posts, setPosts] = useState([])
-  // const [searchResult, setSearchResult] = useState([])
-  
-
-  // useEffect(() => {
-  //   const fetchPosts = async () => {
-  //     const allPosts = await getPosts()
-  //     setPosts(allPosts)
-  //     setSearchResult(allPosts)
-  //   }
-  //   fetchPosts()
-  // }, [])
-
-
-  // const handleSearch = (event) => {
-  //   event.preventDefault()
-  //   const results = posts.filter((post) =>
-  //     post.name.toLowerCase().includes(event.target.value.toLowerCase())
-  //   )
-  //   setSearchResult(results)
-  // }
 
 
   return (
     <Layout user={props.user}>
-      {/* <Search handleSearch={handleSearch} /> */}
       <div className='posts'>
         {searchResult.map((post, index) => {
           return (
