3c9ca0975d1fd7760ab18c86e5fac4aeab41a080
srhodges <89659625+srhodges@users.noreply.github.com>
2021-10-18 14:00:09 -0700

    push

--- seed/data.js
+++ seed/data.js
@@ -10,8 +10,8 @@ const insertData = async () => {
       {
         "title": "post 1",
         "imgURL": "image",
-        "content": "some good stuff",
-        "author": "janet jackson"
+        "content": "It’s quiet here, on the bench. It’s getting dark. I think I see the shadowy things that carried you away. I understand, now, why you wanted to escape. I want to go with them too, sometimes. Would they bring me to you? I won’t go with them, at least not yet. I think things will feel better in the spring, when you return to me, like Persephone. Did I get that right? Anyway, I want to see your tree again, as it was that day, shimmering in its sequin foliage, bending and yearning, growing. Yes, I think things will feel better in the spring, as they always do in the sun.",
+        "author": "Shane Ranson"
       },
       {
         "title": "post 2",
